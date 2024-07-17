4198d5a7ff6c2321ff6edfd554599d731170d511
DESKTOP-AN805P4\user <test@test.com>
2024-07-17 18:54:10 +0900

    2024-07-17 수업 memo 글쓰기(이미지 업로드)

--- src/main/java/com/memo/post/PostRestController.java
+++ src/main/java/com/memo/post/PostRestController.java
@@ -1,5 +1,6 @@
 package com.memo.post;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.memo.post.bo.PostBO;
 
+import jakarta.servlet.http.HttpSession;
+
 @RequestMapping("/post")
 @RestController
 public class PostRestController {
@@ -18,14 +21,26 @@ public class PostRestController {
 	@Autowired
 	private PostBO postBO;
 	
-//	@PostMapping("/create")
-//	public Map<String, Object> create(
-//			@RequestParam("userId") int userId,
-//			@RequestParam("subject") String subject,
-//			@RequestParam("content") String content, 
-//			@RequestParam("file") MultipartFile file) {
-//		
-//		// db insert
-//		post = postBO.addPostCreateByUserId(userId, subject, content, file);
-//	}
+	@PostMapping("/create")
+	public Map<String, Object> create(
+			@RequestParam("subject") String subject,
+			@RequestParam("content") String content,
+			@RequestParam(value = "file", required = false) MultipartFile file,
+			HttpSession session) {  // userId를 받기 위해 session도 받아와야함
+		
+		// 글쓴이 번호를 session에서 꺼낸다 (UserRestController)
+		// 로그인이 되지않은(null값) 상태에서는 오류 발생 int에 null값을 넣을수없으므로
+		int userId = (int)session.getAttribute("userId");
+		String userLoginId = (String)session.getAttribute("userLoginId");
+		
+		// DB insert
+		postBO.addPost(userId, userLoginId, subject, content, file);
+		
+		// 응답값
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 200);
+		result.put("result", "성공");
+		return result;
+
+	}
 }
